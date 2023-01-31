bab5d4dee8219e55f3248634ab296a24632ecce7
Alexandre <alexandre.santacreu@epita.fr>
2023-01-31 21:26:16 +0100

    gravity fixed

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,7 +9,7 @@ public class PlayerBehaviour : MonoBehaviour
 {
 
     public float speed;
-    public float jumpForce;
+    public float jumpForce = 5;
     public Camera cam;
     
     // public float camDist;
@@ -31,7 +31,12 @@ public class PlayerBehaviour : MonoBehaviour
         // Vector3 camPos = new Vector3((float)(-Math.Sin(phi) * Math.Sin(theta)), (float)(Math.Cos(phi) * Math.Sin(theta)), (float)Math.Sin(theta));
         // camPos *= camDist;
         // cam.transform.SetPositionAndRotation(camPos, new Quaternion(0, 0.1f, 0, 1));
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), rb.mass, Input.GetAxis("Vertical")) * speed;
+        //rb.velocity = new Vector3(Input.GetAixs("Horizontal"), rb.velocity.y, Input.GetAxis("Vertical")) * speed;
+        
+        var velo = rb.velocity;
+        velo.x = Input.GetAxis("Horizontal") * speed;
+        velo.z = Input.GetAxis("Vertical") * speed;
+        rb.velocity = velo;
 
         if (Input.GetKeyDown(KeyCode.Space))
             rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
