781428109884f85c1d3017ab21c67034de411b14
Marc-Aurèle <marc-aurele.lepine@epita.fr>
2023-02-23 13:35:39 +0100

    Cam forward move + Loading scenes (both unfinished)

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -12,6 +12,8 @@ public class PlayerBehaviour : MonoBehaviour
     public float jumpForce;
     public float rotationSmoothness;
 
+    public Camera cam;
+
     private Rigidbody rb;
 
     void Awake()
@@ -21,24 +23,39 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update()
     {
+        // Vector3 velo = rb.velocity;
+        // Vector3 moveDir = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+        //
+        // moveDir.Normalize();
+        //
+        // velo.x = Input.GetAxis("Horizontal") * speed;
+        // velo.z = Input.GetAxis("Vertical") * speed;
+        //
+        // rb.velocity = velo;
+        //
+        // if (moveDir != Vector3.zero)
+        // {
+        //     Quaternion targetRot = Quaternion.LookRotation(moveDir, Vector3.up);
+        //     rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, targetRot, rotationSmoothness * Time.deltaTime);
+        // }
+        //
+        // if (Input.GetKeyDown(KeyCode.Space))
+        //     rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        
+        Vector3 vel = rb.velocity;
 
-        Vector3 velo = rb.velocity;
-        Vector3 moveDir = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+        Vector3 forward = cam.transform.forward;
 
-        moveDir.Normalize();
+        forward.Set(forward.x, 0, forward.z);
+        forward *= Input.GetAxis("Vertical");
+        
+        Vector3 side = new Vector3(forward.z, forward.y, forward.x);
+        side *= Input.GetAxis("Horizontal");
+        
+        vel.x = forward.x + side.x;
+        vel.z = forward.z + side.z;
+        
+        rb.velocity = vel;
 
-        velo.x = Input.GetAxis("Horizontal") * speed;
-        velo.z = Input.GetAxis("Vertical") * speed;
-
-        rb.velocity = velo;
-
-        if (moveDir != Vector3.zero)
-        {
-            Quaternion targetRot = Quaternion.LookRotation(moveDir, Vector3.up);
-            rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, targetRot, rotationSmoothness * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
     }
 }
